a471f31fffee3d5ca3e0e2b5f4a3487fe33ca381
jeremylt <jeremylt@users.noreply.github.com>
2020-12-12 01:33:31 +0000

    deploy: 9b2a39088a0a2acfb7312bd47dd942d0f50c3d74

--- advent2020/day11/sidebar-items.js
+++ advent2020/day11/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["NEIGHBORS",""]],"fn":[["game_of_life",""],["part_1",""],["part_2",""],["report",""],["run",""]]});
+initSidebarItems({"constant":[["CAPACITY",""],["NEIGHBORS",""]],"fn":[["game_of_life",""],["part_1",""],["part_2",""],["report",""],["run",""]]});
