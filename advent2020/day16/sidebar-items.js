7c2a4984f66d1cdfed9f9b40151118d0290befb7
jeremylt <jeremylt@users.noreply.github.com>
2020-12-16 21:15:01 +0000

    deploy: 26da83445e473d7821b336df54e362d45f81cf36

--- advent2020/day16/sidebar-items.js
+++ advent2020/day16/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["NUMBER_FIELDS",""]],"fn":[["report",""],["run",""],["valid_field",""]],"struct":[["TicketField",""]]});
+initSidebarItems({"constant":[["NUMBER_FIELDS",""]],"fn":[["report",""],["run",""],["valid_field",""],["valid_fields",""]],"struct":[["TicketField",""]]});
