1c2903db5c3c953e2df2fb1bba78b41127a5bd8a
jeremylt <thompson.jeremy.luke@gmail.com>
2020-12-18 10:11:54 -0700

    reduce its to help repl

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,7 @@ impl Timing {
 // -----------------------------------------------------------------------------
 // Prelude
 // -----------------------------------------------------------------------------
-const REPETITIONS: u32 = 25;
+const REPETITIONS: u32 = 5;
 pub(crate) mod prelude {
     pub(crate) use crate::REPETITIONS;
     pub(crate) use crate::{output, Results, Timing};
