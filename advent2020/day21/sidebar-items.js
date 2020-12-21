ec53b92197e1f1edfee1c9231ed2d3c26a219594
jeremylt <jeremylt@users.noreply.github.com>
2020-12-21 22:49:26 +0000

    deploy: f52f08237454eb9574c98846e1790d8781d96496

---
+++ advent2020/day21/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["CAPACITY",""]],"fn":[["report",""],["run",""]],"struct":[["Food",""]]});
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["advent2020"] = {"name":"","files":["day01.rs","day02.rs","day03.rs","day04.rs","day05.rs","day06.rs","day07.rs","day08.rs","day09.rs","day10.rs","day11.rs","day12.rs","day13.rs","day14.rs","day15.rs","day16.rs","day17.rs","day18.rs","day19.rs","day20.rs","main.rs","output.rs"]};
+sourcesIndex["advent2020"] = {"name":"","files":["day01.rs","day02.rs","day03.rs","day04.rs","day05.rs","day06.rs","day07.rs","day08.rs","day09.rs","day10.rs","day11.rs","day12.rs","day13.rs","day14.rs","day15.rs","day16.rs","day17.rs","day18.rs","day19.rs","day20.rs","day21.rs","main.rs","output.rs"]};
 createSourceSidebar();
