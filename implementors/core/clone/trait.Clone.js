f4ec8e7e561631f7c57c177f17da8d959139c39c
jeremylt <jeremylt@users.noreply.github.com>
2020-12-22 19:21:20 +0000

    deploy: 0c63ec934c4f27db5791371ab71d0b4daec4a87d

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["advent2020"] = [{"text":"impl Clone for Instruction","synthetic":false,"types":[]}];
+implementors["advent2020"] = [{"text":"impl Clone for Instruction","synthetic":false,"types":[]},{"text":"impl Clone for ProductionRule","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
