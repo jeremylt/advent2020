356b6af4c5b0c12a7b86e107f3fe4024c3b336f6
jeremylt <jeremylt@users.noreply.github.com>
2020-12-24 22:30:40 +0000

    deploy: 118d9b2bb69f78966b2fb73241a1aaacfc4bd13a

--- implementors/core/cmp/trait.Eq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["advent2020"] = [{"text":"impl Eq for Coordinate","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
